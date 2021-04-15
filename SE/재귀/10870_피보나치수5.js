4865a2f8b4a678bd841812cae1c7ee32c0d761d2
sieun <najse77@naver.com>
2021-04-15 17:23:24 +0900

    [Pass] boj_10870_피보나치수5

--- SE/재귀/10870_피보나치수5.js
+++ SE/재귀/10870_피보나치수5.js
@@ -2,16 +2,37 @@
 
 const fs = require('fs');
 const input = fs.readFileSync("10870.txt").toString();
-const N = Number(input)
 
-const TC = []
 
-for (let i=0; i <= N; i++) {
-    TC.push(0)
+//// 재귀 삼항연산자로 변형
+function fibo(input) {
+    return input >= 2 ? fibo(input-1) + fibo(input-2) : input;
 }
-TC[1] = 1
-
-for (let i=2; i <=N; i++) {
-    TC[i] = TC[i-1] + TC[i-2]
+for (let i=0; i <= input; i++) {
+    console.log(i, fibo(i))
 }
-console.log(TC[N])
+
+
+//// 기본 재귀
+// function fibonacci(input) {
+//     if (input == 0) return 0;
+//     else if (input == 1) return 1;
+//     else return fibonacci(input-1) + fibonacci(input-2);
+// }
+// console.log(fibonacci(input));
+
+
+
+//// 반복적 동적 계획법 풀이
+// const N = Number(input)
+// const TC = []
+
+// for (let i=0; i <= N; i++) {
+//     TC.push(0)
+// }
+// TC[1] = 1
+
+// for (let i=2; i <=N; i++) {
+//     TC[i] = TC[i-1] + TC[i-2]
+// }
+// console.log(TC[N])
